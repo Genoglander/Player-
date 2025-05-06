8db36ad4cfe61608e4c1e748eb6e75ee90ed8579
Genoglander <chenganniwang@gmail.com>
2025-05-06 22:14:08 +0800

    状态机初版本

---
+++ PlayerStateMachine.cs
@@ -0,0 +1,35 @@
+﻿using Player状态机.状态项目;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInputHandler))]
+public class PlayerStateMachine : MonoBehaviour
+{
+    private IState currentState;
+
+    public PlayerInputHandler InputHandler { get; private set; }
+    public CharacterController Controller { get; private set; }
+
+    private void Awake()
+    {
+        InputHandler = GetComponent<PlayerInputHandler>();
+        Controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        ChangeState(new PlayerIdleState(this));
+    }
+
+    private void Update()
+    {
+        currentState?.OnUpdate();
+    }
+
+    public void ChangeState(IState newState)
+    {
+        currentState?.OnExit();
+        currentState = newState;
+        currentState.OnEnter();
+    }
+
+}
