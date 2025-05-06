8c57d484acbcab502a2d77769476004dba0c4bf0
Genoglander <chenganniwang@gmail.com>
2025-05-06 22:15:12 +0800

    PlayerInputHandler初版本

---
+++ PlayerInputHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player状态机.状态项目
+    using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    public Vector2 MoveInput { get; private set; }
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += ctx => MoveInput = Vector2.zero;
+    }
+
+    private void OnEnable() => inputActions.Enable();
+    private void OnDisable() => inputActions.Disable();
+}
